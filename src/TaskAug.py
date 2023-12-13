Fixed the tensor unused in computation graph issue.

src/TaskAug.py
@@ -78,7 +78,7 @@ def get_hyper_train_flat(hyper_params):
     return torch.cat([p.view(-1) for p in hyper_params])
 
 def gather_flat_grad(loss_grad):
-    return torch.cat([p.reshape(-1) for p in loss_grad if p is not None])
+    return torch.cat([p.reshape(-1) for p in loss_grad])
 
 def neumann_hyperstep_preconditioner(d_val_loss_d_theta, d_train_loss_d_w, elementary_lr, num_neumann_terms, model):
     preconditioner = d_val_loss_d_theta.detach()
@@ -137,10 +137,9 @@ def hyper_step(model, aug, hyper_params, train_loader, optimizer, val_loader, el
 
     preconditioner = neumann_hyperstep_preconditioner(d_val_loss_d_theta, d_train_loss_d_w, elementary_lr,neum_steps, model)
 
-
     indirect_grad = gather_flat_grad(
-        grad(d_train_loss_d_w, hyper_params, grad_outputs=preconditioner.view(-1), allow_unused=True))
-    hypergrad = indirect_grad # + direct_Grad
+        grad(d_train_loss_d_w, hyper_params, grad_outputs=preconditioner.view(-1)))
+    hypergrad = indirect_grad
 
     zero_hypergrad(hyper_params)
     return hypergrad

src/task_aug_operations.py
@@ -45,7 +45,7 @@ class _Operation(nn.Module):
         initial_magnitude = [initial_magnitude] * n_classes
 
 
-        if initial_magnitude is not None and learn_magnitude:
+        if learn_magnitude:
             self.magnitude = nn.Parameter(torch.Tensor(initial_magnitude))
         else:
             self.magnitude = torch.Tensor(initial_magnitude)
@@ -174,9 +174,9 @@ class RandCrop(_Operation):
                  temperature=0.1,
                  num_classes=2
                  ):
-        super().__init__(rand_crop, initial_magnitude, learn_magnitude,
+        super().__init__(rand_crop, initial_magnitude, False,
                          learn_probability, temperature, num_classes)
-        self.magnitude = nn.Parameter(torch.Tensor([initial_magnitude]))
+        self.magnitude = torch.Tensor([initial_magnitude])
 
     def forward(self,input, label):
 